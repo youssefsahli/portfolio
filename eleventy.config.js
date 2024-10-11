ff088e30777269e22537dda68aee2978c78fa6a9
sahli youssef <sahli.youssef@ymail.com>
2024-10-11 17:02:13 +0200

    basic setup

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,7 +1,18 @@
+import pluginIcons from "eleventy-plugin-icons";
+
 export default function (conf) {
 	conf.setLayoutsDirectory("_includes/layouts");
 	conf.addPassthroughCopy("static");
 	conf.addPassthroughCopy("favicon.ico");
 	conf.setServerPassthroughCopyBehavior("passthrough");
 	conf.addNunjucksGlobal("theme-mode", "light");
+	conf.addPlugin(pluginIcons, {
+		sources: [
+			{
+				name: "feather",
+				path: "node_modules/feather-icons/dist/icons",
+				default: true,
+			},
+		],
+	});
 }
